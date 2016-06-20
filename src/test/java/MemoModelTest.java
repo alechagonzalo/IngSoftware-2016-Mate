005356c36782d05da419ebf8a9831be836108cf9
ibado <ignaciobado.unc@gmail.com>
2016-06-20 02:58:17 -0300

    actualizo fichero .gitignore y poco mas

---
+++ src/test/java/MemoModelTest.java
@@ -0,0 +1,63 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ignacio on 19/06/16.
+ */
+public class MemoModelTest {
+
+    private MemoModel model;
+
+    @Before
+    public void setUp()throws Exception{
+
+        model = new MemoModel();
+
+    }
+
+
+    @Test
+    public void modelTest1(){
+        String nombre = model.getNombre(MemoModel.NORMAL);
+        String esperado = "Normal";
+        assertEquals(esperado, nombre);
+    }
+
+    @Test
+    public void modelTest2(){
+        int duracion = model.getDuracion(MemoModel.NORMAL);
+        int expected = 4000;
+        assertEquals("la duracion no es la esperada", expected, duracion,0);
+    }
+
+    @Test
+    public void modelTest3(){
+        for(int i = 0; i < 10; i++){
+            int numero = Integer.parseInt(model.getNextSecuence());
+            assertTrue("algun numero no cumplio la condicion",numero > 10000);
+        }
+    }
+
+    @Test
+    public void modelTest4(){
+        int numero = Integer.parseInt(model.getNextSecuence());
+        boolean condicion = false;
+        for(int i = 0; i < 10; i++){
+            if(numero == Integer.parseInt(model.getNextSecuence()))
+                condicion = false;
+            else
+                condicion = true;
+        }
+        assertTrue(condicion);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        model = null;
+        System.gc();
+
+    }
+}
